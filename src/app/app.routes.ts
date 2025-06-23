69f05051af4523dd825df72dc68d4fcd3b639ea4
Sebastián Damis <sebastian.damis@gmail.com>
2025-06-23 18:29:50 -0300

    3th

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CatalogoComponent } from './catalogo/catalogo.component';
+import { DetalleComponent } from './detalle/detalle.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {   
+        path: "home",
+        component: HomeComponent
+    },
+    {   
+        path: "**",
+        component: HomeComponent
+    },
+    {   
+        path: "catalogo",
+        component: CatalogoComponent
+    },
+    {   
+        path: "detalle",
+        component: DetalleComponent
+    },
+    {   
+        path: "login",
+        component: LoginComponent
+    },
+    {   
+        path: "registro",
+        component: RegistroComponent
+    },
+];
